590766148e90213b625a27afaae14d76693962c4
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2025-02-24 22:51:30 -0300

    feat: Implementando as classes CLIENTE para acessar outros microsserviços (CLIENT)

--- hexagonal/src/main/java/br/com/dev/thiagomds/hexagonal/HexagonalApplication.java
+++ hexagonal/src/main/java/br/com/dev/thiagomds/hexagonal/HexagonalApplication.java
@@ -2,7 +2,9 @@ package br.com.dev.thiagomds.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HexagonalApplication {
 
---
+++ hexagonal/src/main/java/br/com/dev/thiagomds/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,17 @@
+package br.com.dev.thiagomds.hexagonal.adapters.out.client;
+
+import br.com.dev.thiagomds.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "FindAddressByZipCodeClient",
+        url  = "${spring.application.client-http.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/address/{zipCode}")
+    AddressResponse findAddress(@PathVariable("zipCode") String zipCode);
+
+}
