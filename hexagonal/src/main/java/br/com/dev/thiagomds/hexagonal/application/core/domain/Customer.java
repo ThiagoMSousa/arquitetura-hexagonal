9a4a46008615f4a139cf179c044147c79033d0bc
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2025-02-24 22:00:16 -0300

    feat: Implementando as classes DOMAIN

---
+++ hexagonal/src/main/java/br/com/dev/thiagomds/hexagonal/application/core/domain/Customer.java
@@ -0,0 +1,33 @@
+package br.com.dev.thiagomds.hexagonal.application.core.domain;
+
+public class Customer {
+
+    private String id;
+    private String name;
+    private Address address;
+    private String cpf;
+    private Boolean isValidCPF;
+
+    public Customer() {
+        setValidCPF(false);
+    }
+
+    public Customer(String id, String name, Address address, String cpf, Boolean isValidCPF) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cpf = cpf;
+        this.isValidCPF = isValidCPF;
+    }
+
+    public String getId()                     { return id; }
+    public void setId(String id)              { this.id = id; }
+    public String getName()                   { return name; }
+    public void setName(String name)          { this.name = name; }
+    public Address getAddress()               { return address; }
+    public void setAddress(Address address)   { this.address = address; }
+    public String getCpf()                    { return cpf; }
+    public void setCpf(String cpf)            { this.cpf = cpf; }
+    public Boolean getValidCPF()              { return isValidCPF; }
+    public void setValidCPF(Boolean validCPF) { isValidCPF = validCPF; }
+}
